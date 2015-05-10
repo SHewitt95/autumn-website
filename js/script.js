494817a9827611fdc0e13088dd3fbca259809f3a
Sherman Hewitt <s.hewitt7@umiami.edu>
2015-05-10 15:56:21 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/*var arrows = document.getElementsByClassName("arrow");
+var slider = document.getElementById("slider");*/
+
+var currentPhoto = 0;
+var maxPhoto = 18;
+
+function changeSlide(number) {
+  // Fetches img tag in slider.
+  var image = document.getElementById("img-in-slider");
+
+  // Adds number (either 1 or -1) to currentPhoto.
+  currentPhoto = currentPhoto + number;
+
+  // Corrects currentPhoto if it exceeds # of total photos or is negative.
+  if (currentPhoto < 0) {
+    currentPhoto = maxPhoto;
+  } else if (currentPhoto > maxPhoto) {
+    currentPhoto = 0;
+  }
+
+  // Appends new currentPhoto number to the img tag in slider.
+  var srcText = "img/slider-img/autumn" + currentPhoto + ".jpg";
+  image.src = srcText;
+}
